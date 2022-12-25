b7e7a73bc768007adfb6be69fbd3f3a88df625ae
Ismael <84606443+ismaaadc@users.noreply.github.com>
2022-12-25 20:29:40 +0100

    init

---
+++ gif-expert-app/src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+import './index.css'
+
+const root = document.getElementById("root")
+
+ReactDOM.createRoot(root).render(
+    <>
+        
+    </>
+)
