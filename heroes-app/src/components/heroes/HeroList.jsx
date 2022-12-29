9ee9ef27fa9d35505df138179624a997798e30df
Ismael <84606443+ismaaadc@users.noreply.github.com>
2022-12-29 17:25:10 +0100

    Hero List component

---
+++ heroes-app/src/components/heroes/HeroList.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher'
+
+export const HeroList = ({publisher}) => {
+ 
+    
+ const heroes = getHeroesByPublisher(publisher)
+  return (
+    <>
+        <ul>
+            {
+                heroes.map(heroe => {
+                    return <li key={heroe.id}>{ heroe.superhero }</li>
+                })
+            }
+        </ul>
+    </>
+  )
+}
